1fec6bae0aaae0896a12dc2a25f887a795772b20
Pariyul <pariyuljan.com>
2024-09-26 17:29:53 +0530

    first hook done

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,10 @@ function App() {
   const [name, setName]= useState("");
   const [interest, setInterest] = useState("");
 
+  const Changename=(e) =>{
+    setName(e.target.value);
+  }
+
   return (
     <>
       <div className="flex justify-center items-center ">
@@ -20,9 +24,27 @@ function App() {
           <h1>Github Readme Generator</h1>
         </div>
       </div>
-      <div className="container bg-cyan-500  h-full w-[95vw] items-start">
-        <div className="">
-          <h1>Header</h1>
+      <div className="container  h-full w-[95vw] items-start bg-gradient-to-r from-[#2b5876] to-[#4e4376] mt-5 mb-5 mx-auto flex justify-evenly rounded-xl break-words">
+
+        <div className=" lcon w-[45%] py-[100px] flex flex-col space-y-4">
+          <div className="lcon1 bg-white h-[53%] rounded-lg p-[20px]">
+          <h1 className='text-3xl font-bold underline decoration-purple-400'>Header</h1>
+          <h3 className='block text-gray-700 font-bold'>Heading</h3>
+          <input className='border-2	border-gray-200' type="text" placeholder='Enter your name'value={name} onChange={Changename} />
+          </div>
+
+          <div className="lcon2 lcon1 bg-white h-[53%] p-[20px]">
+            <h1 className='text-3xl font-bold underline decoration-purple-400'>Preview</h1>
+            {name === "" ? (
+              <p>Hello! Myself Pariyul Jain</p>
+            ):(
+              <p>Hello! Myself ,{" "}
+              <span>
+                {name}
+              </span>
+              </p>
+            ) }
+          </div>
         </div>
       </div>
     </>
